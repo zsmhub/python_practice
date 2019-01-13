f202e0f35b5a3e31705234a55105c7b24f9a5402
zsmhub <zhangshimiande@gmail.com>
2019-01-13 22:14:54 +0800

    添加多进程示例

--- email_send.py
+++ email_send.py
@@ -1,32 +1,35 @@
-# _*_ coding: utf-8 _*_
-
+# -*- coding: utf-8 -*-
 '''
 使用qq邮箱发送邮件
 '''
 
-from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
-
 import smtplib
 
+
 def _format_addr(s):
+    '''
+    收发件人地址格式化
+    '''
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
+
 
 from_addr = '你的qq邮箱'
 password = '你的授权码'
 to_addr = '收件邮箱'
 smtp_server = 'smtp.qq.com'
+smtp_port = 465
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg = MIMEText('hello, send by Python开发者...', 'plain', 'utf-8')
 msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 
-server = smtplib.SMTP_SSL(smtp_server, 465)
-server.set_debuglevel(1)
+server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+# server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
--- manhuatai_thread/common.py
+++ manhuatai_thread/common.py
@@ -2,38 +2,39 @@
 
 import os, time
 
+
 class Common(object):
-    '''
+    """
     一些通用函数
-    '''
+    """
 
     def __init__(self):
         pass
 
     def mkdir(self, path):
-        '''
+        """
         创建文件夹
 
         Args:
             path: 目录路径
-        '''
+        """
         if not os.path.exists(path):
             os.mkdir(path)
 
-    def removedirs(sef, path):
-        '''
+    def removedirs(self, path):
+        """
         递归删除文件夹
 
         Args:
             path: 目录路径
-        '''
+        """
         if os.path.exists(path):
             os.removedirs(path)
 
     def get_time(self):
-        '''
+        """
         获取当前时间并返回
-        '''
+        """
         timestamp = time.time()
         time_local = time.localtime(timestamp)
         datetime = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
